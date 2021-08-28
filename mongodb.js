1b4fb2ef1c3744a6e5e91d1c7079d7f2b6bb507e
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-28 19:57:16 +0800

    ✨ Insert data and objectId

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,11 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectId();
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -14,9 +17,48 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Gabriel",
-      age: 24,
-    });
+    db.collection("users").insertOne(
+      {
+        _id: id,
+        name: "Andy",
+        age: 27,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+
+        console.log(result.insertedId);
+      }
+    );
+
+    db.collection("users").insertMany(
+      [
+        { name: "Jen", age: 28 },
+        { name: "Gunther", age: 27 },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents");
+        }
+
+        console.log(result.insertedIds);
+      }
+    );
+
+    db.collection("tasks").insertMany(
+      [
+        { description: "task 1", completed: true },
+        { description: "task 2", completed: false },
+        { description: "task 3", completed: true },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert tasks");
+        }
+
+        console.log(result.insertedIds);
+      }
+    );
   }
 );
