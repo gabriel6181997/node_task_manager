5c7b82bcfbb2f5abc3c8737ae5b487a97287b4a0
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-29 17:28:00 +0800

    ✨ Create reading endpoints

--- src/index.js
+++ src/index.js
@@ -21,6 +21,31 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+  console.log(req.params);
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
@@ -33,6 +58,30 @@ app.post("/tasks", (req, res) => {
       res.status(400).send(error);
     });
 });
+
+app.get("/tasks", (req,res)=> {
+  Task.find({})
+  .then((tasks)=>{
+    res.send(tasks);
+  })
+  .catch((error)=>{
+    res.status(500).send();
+  });
+})
+
+app.get("/tasks/:id", (req,res)=> {
+  const _id = req.params.id;
+  Task.findById(_id)
+  .then((task)=>{
+    if(!task){
+      return res.status(404).send();
+    }
+    res.send(task);
+  })
+  .catch((error)=> {
+    res.status(500).send();
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is up on port${port}`);
