83366610f3627353e47306f0815e5f2996a82927
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-29 18:27:29 +0800

    ♻️ Rewrite promise-chaining code by using async awit

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,13 +2,26 @@ require("../src/db/mongoose");
 const User = require("../src/models/user");
 
 //612b2d18acad16fa471cbcb2
-User.findByIdAndUpdate("612b3c29b9097e3b960af460", { age: 1 }).then((user) => {
-  console.log(user);
-  return User.countDocuments({age:1})
-}).then((result)=> {
-  console.log(result)
+// User.findByIdAndUpdate("612b3c29b9097e3b960af460", { age: 1 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id,{age});
+  const count = await User.countDocuments({age});
+  return count;
+};
+
+updateAgeAndCount("612b3c29b9097e3b960af460", 2).then((count)=> {
+  console.log(count);
 }).catch((error)=> {
   console.log(error)
 })
-
-
