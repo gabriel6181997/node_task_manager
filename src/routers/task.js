a3e8976688e5c27353643e766ade8881e1069323
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-31 23:19:57 +0800

    ✨ Add Sort function

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,6 +19,12 @@ router.post("/tasks", auth, async (req, res) => {
 
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
 
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
@@ -31,6 +37,7 @@ router.get("/tasks", auth, async (req, res) => {
       options: {
         limit: parseInt(req.query.limit),
         skip: parseInt(req.query.skip),
+        sort,
       },
     });
     res.send(req.user.tasks);
