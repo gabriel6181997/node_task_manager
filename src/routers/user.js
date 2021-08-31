76cf7a1acacb6dd03f958e572458ff273873f4c8
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-31 18:52:47 +0800

    ✨ Create logout API endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,7 +9,7 @@ router.post("/users", async (req, res) => {
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    res.status(201).send({user, token});
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -25,6 +25,30 @@ router.post("/users/login", async (req, res) => {
     res.send({ user, token });
   } catch (error) {
     res.status(400).send();
+  }
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
