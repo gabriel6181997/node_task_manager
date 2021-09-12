4b3843f50a4e65ef87bfe57e43da7deb2d324045
gabriel6181997 <gabriel6181997@gmail.com>
2021-09-12 16:41:19 +0800

    🏷️ Update interfaces

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,16 +1,34 @@
-import mongoose, { Document } from "mongoose";
+import mongoose, { Document, Model } from "mongoose";
 
-export interface UserModel extends Document {
+export interface UserDocument extends Document {
   name: string;
   email: string;
   password: string;
   age?: number;
-  tokens: string[];
+  tokens? : {[key: string]: string}[];
+  generateAuthToken: () => string;
+  toJSON: <UserDocument>() => UserDocument;
+  task: {[key: string]: string | boolean}[]
   avatar?: BufferConstructor;
 }
 
-export interface TaskModel extends Document {
+export interface UserModel extends Model<UserDocument> {
+  findByCredentials: (email: string, password: string) => UserDocument;
+}
+
+export interface TaskDocument extends Document {
   description: string;
   completed?: boolean;
   owner: mongoose.Schema.Types.ObjectId;
 };
+
+export type DecodedToken = { _id: string; iat: number };
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: UserDocument;
+      token: string;
+    }
+  }
+}
